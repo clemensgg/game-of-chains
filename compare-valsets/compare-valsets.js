bd2ce30447fb34cc64ee808f2aed7e83cead6fee
clemensgg <csc@soundportal.at>
2022-11-17 11:35:20 +0100

    alerter bugfix

--- compare-valsets/compare-valsets.js
+++ compare-valsets/compare-valsets.js
@@ -32,7 +32,6 @@ async function writeFile(path, data) {
         await fs.writeFile(path, data);
     }
     catch (e) {
-        console.log('> ERROR writing blacklist to filesystem');
         console.log(e.data);
         return false;
     }
@@ -60,6 +59,17 @@ async function dumpValsetRecords() {
     return true;
 }
 
+function isUniqueHash(chain, hash) {
+    let len = chain.valset_data.length;
+    let unique_hash = true;
+    for (var i = 0; i < len; i++) {
+        if (chain.valset_data[i][1] == hash) {
+            unique_hash = false;
+        }
+    }
+    return unique_hash;
+}
+
 // fetch latest valset
 async function fetchLatestValset(chain) {
     try {
@@ -70,6 +80,38 @@ async function fetchLatestValset(chain) {
         return false;
     }
     return res.data.result;
+}
+
+// fetch new valset
+async function fetchNewValset(chain) {
+    let res = await fetchLatestValset(chain);
+    let height = res.block_height;
+    if (height > chain.last_height) {
+        console.log(chain.id + ' block: ' + res.block_height)
+        let valset = res.validators;
+        let complete_set = completeSet(valset, height);
+        chain.last_height = height;
+        return complete_set;
+    }
+    return false;
+}
+
+// create complete valset object for csv output
+function completeSet(valset, height) {
+    let valset_export = [];
+    let total_vp = 0;
+    let complete_set = {
+        "validators": {}
+    };
+    valset.forEach((validator) => {
+        valset_export.push(validator.address, validator.voting_power);
+        complete_set.validators[validator.address] = validator.voting_power;
+        total_vp = total_vp + parseFloat(validator.voting_power);
+    });
+    complete_set.total_vp = total_vp;
+    complete_set.height = height;
+    complete_set.sha256hash = hash(valset_export.toString());
+    return complete_set;
 }
 
 // append new valset of chain
@@ -104,47 +146,16 @@ function appendSet(chain, set) {
     return;
 }
 
-function completeSet(valset, height) {
-    let valset_export = [];
-    let total_vp = 0;
-    let complete_set = {
-        "validators": {}
-    };
-    valset.forEach((validator) => {
-        valset_export.push(validator.address, validator.voting_power);
-        complete_set.validators[validator.address] = validator.voting_power;
-        total_vp = total_vp + parseFloat(validator.voting_power);
-    });
-    complete_set.total_vp = total_vp;
-    complete_set.height = height;
-    complete_set.sha256hash = hash(valset_export.toString());
-    return complete_set;
-}
-
-// fetch new valset
-async function fetchNewValset(chain) {
-    let res = await fetchLatestValset(chain);
-    let height = res.block_height;
-    if (height > chain.last_height) {
-        console.log(chain.id + ' block: ' + res.block_height)
-        let valset = res.validators;
-        let complete_set = completeSet(valset, height);
-        chain.last_height = height;
-        return complete_set;
-    }
-    return false;
-}
-
 async function alertAndDumpSet(cc_new_set) {
     // alert log
     console.log('> DIFFERING VALSETS DETECTED!')
     console.log('PC height: ' + provider.last_height);
     console.log('CC height: ' + cc_new_set.height);
     console.log('CC set ' + cc_new_set.sha256hash + ' not found in historic valsets of ' + provider.id + ' !')
-    console.log('CC dumping ' + consumer.id + ' set');
-    console.log(JSON.stringify(cc_new_set.valset));
+    console.log('CC dumping ' + cc_new_set.height + ' set');
+    console.log(JSON.stringify(cc_new_set));
     // save valset to disk
-    dumpValsetRecords;
+    dumpValsetRecords();
     return true;
 }
 
@@ -166,17 +177,6 @@ async function compareLiveValsets() {
         }
     }
     return;
-}
-
-function isUniqueHash(chain, hash) {
-    let len = chain.valset_data.length;
-    let unique_hash = true;
-    for (var i = 0; i < len; i++) {
-        if (chain.valset_data[i][1] == hash) {
-            unique_hash = false;
-        }
-    }
-    return unique_hash;
 }
 
 // fetch historic valsets of PC
